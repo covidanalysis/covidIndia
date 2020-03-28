10e7bcb0cda903c0280a263d051460d1879e08e3
reetesh.mukul <reetesh.mukul@gmail.com>
2020-03-28 20:27:27 +0530

    Covid Basic Graph

--- graph_analysis/CovidData.py
+++ graph_analysis/CovidData.py
@@ -1,8 +1,13 @@
 import json
 import urllib
+import pandas as pd
+import itertools
+import networkx as nx
 
 class CovidData:
     _covid_19_url = "https://api.rootnet.in/covid19-in/unofficial/covid19india.org"
+    graph = None
+    dframe = None
     
     def get_covid_data_request(self):
         covid_19_req = urllib.request.Request(
@@ -18,4 +23,28 @@ class CovidData:
         req = self.get_covid_data_request()
         with urllib.request.urlopen(req) as url:
             data = json.loads(url.read().decode())
-        return data    
+        return data
+    
+    def get_and_set_raw_data_frame(self):
+        if self.dframe:
+            return self.dframe
+        covid_json = self.get_covid_json_data()
+        dframe = pd.DataFrame.from_dict(covid_json["data"]["rawPatientData"])
+        dframe["pid"] = dframe["patientId"].apply(lambda x: "P" + str(x))
+        def to_edge(pid, links):
+            return list(itertools.chain(*[[(pid,q) for q in qs["with"]] for qs in links]))
+        dframe["edges"] = dframe.apply(lambda x: to_edge(x.pid, x.relationship), axis=1)
+        self.dframe = dframe
+        return self.dframe
+    
+    def get_patient_graph(self):
+        if self.graph:
+            return self.graph
+        
+        self.get_and_set_raw_data_frame( )
+        
+        edges = list(itertools.chain(*self.dframe.edges))
+        G = nx.Graph()
+        G.add_edges_from(edges)
+        self.graph = G
+        return self.graph
--- graph_analysis/covid_graph.py
+++ graph_analysis/covid_graph.py
@@ -1,6 +1,9 @@
 # %%
-from graph_analysis import CovidData
-
-print(CovidData.CovidData().get_covid_json_data())
+import pandas as pd
+import matplotlib as plt
+import networkx as nx
+from graph_analysis.CovidData import CovidData
+covidgraph = CovidData().get_patient_graph()
+nx.draw(covidgraph)
 
 # %%
