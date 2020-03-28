a9e532bb7758ed16cd255bcd411b03d251a1f57a
reetesh.mukul <reetesh.mukul@gmail.com>
2020-03-28 20:53:06 +0530

    with graph mean degree

--- graph_analysis/CovidDataGraph.py
+++ graph_analysis/CovidDataGraph.py
@@ -3,8 +3,9 @@ import urllib
 import pandas as pd
 import itertools
 import networkx as nx
+import numpy as np
 
-class CovidData:
+class CovidDataGraph:
     _covid_19_url = "https://api.rootnet.in/covid19-in/unofficial/covid19india.org"
     graph = None
     dframe = None
@@ -19,15 +20,13 @@ class CovidData:
         )
         return covid_19_req
     
-    def get_covid_json_data(self):
+    def _fetch_covid_json_data( self ):
         req = self.get_covid_data_request()
         with urllib.request.urlopen(req) as url:
             data = json.loads(url.read().decode())
-        return data
-    
-    def get_and_set_raw_data_frame(self):
-        if self.dframe:
-            return self.dframe
+        self._json_data = data
+        
+    def _set_data_frame(self):
         covid_json = self.get_covid_json_data()
         dframe = pd.DataFrame.from_dict(covid_json["data"]["rawPatientData"])
         dframe["pid"] = dframe["patientId"].apply(lambda x: "P" + str(x))
@@ -35,16 +34,31 @@ class CovidData:
             return list(itertools.chain(*[[(pid,q) for q in qs["with"]] for qs in links]))
         dframe["edges"] = dframe.apply(lambda x: to_edge(x.pid, x.relationship), axis=1)
         self.dframe = dframe
+        return self.dframe   
+     
+    def _set_patient_graph(self):    
+        self.get_data_frame( )  
+        edges = list(itertools.chain(*self.dframe.edges))
+        G = nx.Graph()
+        G.add_nodes_from(list(self.dframe.pid))
+        G.add_edges_from(edges)
+        self.graph = G
+        return self.graph   
+    
+    def get_covid_json_data(self):
+        return self._json_data
+    
+    def get_data_frame(self):
         return self.dframe
     
     def get_patient_graph(self):
-        if self.graph:
-            return self.graph
-        
-        self.get_and_set_raw_data_frame( )
-        
-        edges = list(itertools.chain(*self.dframe.edges))
-        G = nx.Graph()
-        G.add_edges_from(edges)
-        self.graph = G
         return self.graph
+    
+    def __init__(self):
+        self._fetch_covid_json_data()
+        self._set_data_frame()
+        self._set_patient_graph()
+    
+    def get_cluster_mean_degree(self):
+        return np.mean([d for n, d in self.graph.degree()])
+
--- graph_analysis/covid_graph.py
+++ graph_analysis/covid_graph.py
@@ -2,8 +2,6 @@
 import pandas as pd
 import matplotlib as plt
 import networkx as nx
-from graph_analysis.CovidData import CovidData
-covidgraph = CovidData().get_patient_graph()
-nx.draw(covidgraph)
-
-# %%
+from graph_analysis.CovidDataGraph import CovidDataGraph
+cvd = CovidDataGraph()
+print(cvd.get_cluster_mean_degree())
